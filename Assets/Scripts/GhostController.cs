14b087d28680195586996a9a264461a540ab2db5
seescode <seesyong@gmail.com>
2016-03-03 22:36:47 -0500

    move toward pacman

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -4,14 +4,40 @@ using System.Collections;
 public class GhostController : MonoBehaviour
 {
 	public Transform pacman;
-	
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		//Make this ghost rotate to look at pacman
 		Vector3 relativePos = pacman.position - transform.position;
-		transform.rotation = Quaternion.LookRotation(relativePos);
+		//transform.rotation = Quaternion.LookRotation(relativePos);
 
 		//Move forward based on rotation
-		transform.position += transform.forward * Time.deltaTime;
+		///transform.position += transform.forward * Time.deltaTime;
+
+		Vector3 direction = relativePos * Time.deltaTime;
+
+		RaycastHit hit;
+		Ray landingRay = new Ray(rb.transform.position, direction);
+
+		if (Physics.Raycast(landingRay, out hit, 0.5f))
+		{
+			if (hit.collider.gameObject.tag != "Wall")
+			{
+				transform.Translate(/*rotation * */ direction);
+			}
+		}
+		else
+		{
+			//Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+			transform.Translate(/*rotation * */ direction);
+		}
+
 	}
 }
